04fc712ebd64eff9dcbdff7083dc0b3bd0bb3a45
ZZIDZZ <zidanutomo@gmail.com>
2021-11-20 06:46:30 +0700

    added index.html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "Hello World, by Flask Web Server"
+    render_template('index.html')
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
